d7a041f17e4d3dd52113feb0130ff0085406bd2c
xvw <xavier.vdw@gmail.com>
2016-03-07 00:14:41 +0100

    Complete readme

--- examples/example.ml
+++ examples/example.ml
@@ -3,3 +3,12 @@
 type cm [@@measure]
 and  m  [@@measure fun cm -> cm *. 100.  ]
 and  km [@@measure fun cm -> cm *. 1000. ]
+
+
+type cl [@@measure]
+and  l  [@@measure fun cl -> cl *. 100.  ]
+and  kl [@@measure fun cl -> cl *. 1000. ]
+
+
+type euro   [@@measure]
+and  dollar [@@measure fun euro -> euro /. 1.0993 ] 
